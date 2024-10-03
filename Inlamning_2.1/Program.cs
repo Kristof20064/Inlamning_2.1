b2671fa92f1263914681f7329b4818ed1640f663
krbu06001 <krbu06001@E-PW05CGQY>
2024-10-03 13:43:58 +0200

    Add project files.

---
+++ Inlamning_2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace inlamning_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            string bok = Console.ReadLine();
+
+            Console.WriteLine(bok);
+        }
+    }
+}
